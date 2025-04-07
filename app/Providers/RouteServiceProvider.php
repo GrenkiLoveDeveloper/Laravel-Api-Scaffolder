2110eaa215b9ed5a2f8570b7e44544e24a3a3170
Danil <46190654+GrenkiLoveDeveloper@users.noreply.github.com>
2025-04-07 19:43:26 +0300

    fix: RouteServiceProvider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -9,15 +9,13 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 
-class RouteServiceProvider extends ServiceProvider
-{
+class RouteServiceProvider extends ServiceProvider {
     public const string HOME = '/';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
      */
-    public function boot(): void
-    {
+    public function boot(): void {
 
         // ограничение количества запросов
         $this->configureRateLimiting();
@@ -32,19 +30,12 @@ class RouteServiceProvider extends ServiceProvider
     /**
      * Ограничение количества запросов в минуту.
      */
-    protected function configureRateLimiting(): void
-    {
-        // RateLimiter::for('api', function ($request) {
+    protected function configureRateLimiting(): void {
 
-        //     return Limit::perMinute(160)->response(function ($request) {
+        RateLimiter::for('api', function ($request) {
+            $userId = $request->user() ? $request->user()->id : null;
 
-        //         Log::warning('Rate limit exceeded for user', [
-        //             'ip' => $request->ip(),
-        //             'user_id' => optional($request->user())->id,
-        //         ]);
-
-        //         return response('Too many requests', 429);
-        //     });
-        // });
+            return Limit::perMinute(100)->by($userId ?? $request->ip());
+        });
     }
 }
